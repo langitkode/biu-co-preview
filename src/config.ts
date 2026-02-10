d8eb11aa00fe631494c31b68850a4ceecd5c0478
GitHub Action <action@github.com>
2026-02-10 11:15:26 +0000

    Initial commit from UMKM Factory

---
+++ src/config.ts
@@ -0,0 +1,81 @@
+import { UMKMConfig } from "./types/config";
+
+export const config: UMKMConfig = {
+  "id": "biu-co",
+  "businessName": "Biu.Co",
+  "tagline": "Brand Factory Outlet Store",
+  "description": "Pakaian wanita trendy",
+  "templateId": "template-10",
+  "heroImage": "https://down-id.img.susercontent.com/file/id-11134207-8224r-mhitgny461adda@resize_w900_nl.webp",
+  "theme": {
+    "primaryColor": "#DC2626",
+    "secondaryColor": "#000000"
+  },
+  "contact": {
+    "address": " Jl Raya Candrabaga No 5, Babelan Kabupaten Bekasi (Marakash)",
+    "gmapsUrl": "https://maps.app.goo.gl/xhyATofaChuLRSkVA",
+    "whatsapp": "6281912345647‬",
+    "instagram": "biu.costorebekasi",
+    "openHours": "Mon-Sun, 08:00 - 20:00"
+  },
+  "features": {
+    "enableCart": false,
+    "enableSearch": false
+  },
+  "products": [
+    {
+      "id": "p1768116841973",
+      "link": "https://shopee.co.id/korean-style-kode-B-co-live-biu.co-pakaian-wanita-ala-korea-baju-bangkok-i.1594310297.53751650889?extraParams=%7B%22display_model_id%22%3A244093745750%7D",
+      "name": "pakaian wanita ala korea",
+      "image": "https://down-id.img.susercontent.com/file/id-11134207-82251-mhijw5c90h6u3b@resize_w900_nl.webp",
+      "price": 80000,
+      "category": "Baju",
+      "description": "korean style kode B co live biu.co pakaian wanita ala korea baju bangkok"
+    },
+    {
+      "id": "p1768117549848",
+      "link": "https://shopee.co.id/Pakain-Wanita-Ala-Korean-Style-Blouse-Katbol-Kemeja-Lengan-Panjang-i.1594310297.51352136546?extraParams=%7B%22display_model_id%22%3A315207957421%7D",
+      "name": "Kemeja Lengan Panjang",
+      "image": "https://down-id.img.susercontent.com/file/id-11134207-8224r-mhitgny461adda@resize_w900_nl.webp",
+      "price": 300000,
+      "category": "Kemeja",
+      "description": "Pakain Wanita Ala Korean Style Blouse Katbol Kemeja Lengan Panjang"
+    },
+    {
+      "id": "p1768117598708",
+      "link": "https://shopee.co.id/BIU.co-Rok-Celana-Silvia-(Mini)-i.1594310297.42612154455?extraParams=%7B%22display_model_id%22%3A301018134358%7D",
+      "name": "Rok Celana Silvia",
+      "image": "https://down-id.img.susercontent.com/file/id-11134207-7ra0l-md8eptrdiyt127.webp",
+      "price": 138000,
+      "category": "Rok",
+      "description": "Rok Celana Silvia (Mini)\n"
+    }
+  ],
+  "testimonials": [
+    {
+      "id": "t1768116947371",
+      "rating": 5,
+      "comment": "Bajunya bagus",
+      "customerName": "Bayu"
+    },
+    {
+      "id": "t1768117689070",
+      "rating": 5,
+      "comment": "Bahannya adem",
+      "customerName": "Dimas"
+    }
+  ],
+  "instagramImages": [],
+  "includedTemplates": [
+    "template-10",
+    "template-6",
+    "template-3"
+  ],
+  "cta": {
+    "link": "",
+    "text": "Order Now"
+  },
+  "footer": {
+    "copyrightText": "© 2026 BIU.CO. All Rights Reserved."
+  }
+};
